193318b27502f9b62909f42a1aec098ed88091ee
laboye <31959459+laboye@users.noreply.github.com>
2017-12-15 00:50:43 -0500

    Initial commit

---
+++ MvcMovie/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { CreateMovieComponent } from './components/movies/createmovie.component';
+import { DeleteMovieComponent } from './components/movies/deletemovie.component';
+import { EditMovieComponent } from './components/movies/editmovie.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        MoviesComponent,
+        CreateMovieComponent,
+        DeleteMovieComponent,
+        EditMovieComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'movies', component: MoviesComponent },
+            { path: 'createmovie', component: CreateMovieComponent },
+            { path: 'deletemovie/:id', component: DeleteMovieComponent },
+            { path: 'editmovie/:id', component: EditMovieComponent},
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
---
+++ MvcMovie/ClientApp/app/components/movies/movies.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'movies',
+    templateUrl: './movies.component.html',
+    styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent {
+    public movies: Movie[];
+    public moviesBak: Movie[];
+    public searchTerm: string;
+
+    constructor(public http: Http, @Inject('BASE_URL') public baseUrl: string) {
+        http.get(baseUrl + 'api/Movies').subscribe(result => {
+            this.movies = result.json() as Movie[];
+            this.moviesBak = this.movies;
+        }, error => console.error(error));
+    }
+
+    filter() {
+        this.movies = this.moviesBak;
+
+        if (this.searchTerm ===""){
+            //Do nothing 
+        } else {
+            this.movies = this.movies.filter(m => m.title.includes(this.searchTerm) || m.genre.includes(this.searchTerm));
+        }
+    }
+}
+interface Movie {
+    id: number;
+    title: string;
+    releaseDate: number;
+    genre: string;
+    price: number;
+    rating: string;
+}
